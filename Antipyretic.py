feb322bbf58c71a3388518d855e6f3856a0f9534
Arctind <37456899+Arctind@users.noreply.github.com>
2023-11-27 00:48:49 -0600

    Add files via upload
    Added a GUI

--- Antipyretic.py
+++ Antipyretic.py
@@ -1,5 +1,8 @@
 import requests
 import time
+import threading
+from tkinter import *
+from PIL import Image, ImageTk
 from selenium import webdriver
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
@@ -9,6 +12,19 @@ from requests_html import HTMLSession
 timeList = []
 actionList = []
 damageList = []
+input = "https://www.fflogs.com/reports/QVT2MqxGPrvRbk4J#fight=30&type=damage-done&source=7&view=events"
+
+master = Tk()
+master.resizable(False, False)
+canvas = Canvas(master, width=960, height=540)
+canvas.pack()
+
+bg = ImageTk.PhotoImage(file="bg.jpg")
+canvas.create_image(0, 0, image=bg, anchor=NW)
+
+entry = Entry(master, width=100, font=("Helvetica, 12"), bg="#7DC3E1")
+entry.place(relx=.5, rely=.5, anchor=CENTER)
+entry.insert(0, "Enter log URL")
 
 def renderPage(URL):
     global driver
@@ -21,7 +37,6 @@ def renderPage(URL):
 
 def parser():
     global content
-    i = 0
     content = driver.find_element(By.CSS_SELECTOR, "div[class*='event-view-events-box'")
     content = content.find_element(By.TAG_NAME, "tbody")
     time = content.find_elements(By.CSS_SELECTOR, "td[class*='main-table-number'")
@@ -36,10 +51,19 @@ def parser():
         damageList.append(d.text)
     return
 
-if __name__ == "__main__":
-    renderPage("https://www.fflogs.com/reports/QVT2MqxGPrvRbk4J#fight=30&type=damage-done&source=7&view=events")
+def main():
+    input = entry.get()
+    renderPage(input)
     parser()
     print(content)
     print(timeList)
     print(actionList)
     print(damageList)
+
+if __name__ == "__main__":
+    button = Button(master, text="Go", command=main)
+    button.pack()
+    #threading.Thread(target=main())
+    master.mainloop()
+    #renderPage(entry)
+    #parser()
